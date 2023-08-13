d4b021fb0b14db1b30b6b10500e745c7e2cd8b1d
DvirEzra <122629905+DvirEzra@users.noreply.github.com>
2023-08-13 08:06:45 +0300

    InactiveUserReport

---
+++ InactiveUserReport.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module ActiveDirectory
+
+$DaysInactive = 90
+$Time = (Get-Date).AddDays(-$DaysInactive)  
+
+Get-ADUser -Filter { LastLogonTimeStamp -lt $Time -and enabled -eq $true } -Properties * | Select-Object Name, LastLogonDate | Export-Csv "C:\temp\InactiveUsers.csv" -Encoding UTF8 -NoTypeInformation
+
+# Send Email
+$smtpServer = "smtp.office365.com"
+$smtpFrom = "reports@company.com"
+$smtpTo = "admin@company.com"  
+
+$message = new-object Net.Mail.MailMessage
+$message.From = $smtpFrom
+$message.To.Add($smtpTo)
+$message.Subject = "Inactive Users Report"
+$message.Body = "Attached is the monthly inactive users report."
+$message.Attachments.Add("C:\temp\InactiveUsers.csv")
+
+$smtp = new-object Net.Mail.SmtpClient($smtpServer)
+$smtp.Send($message)
